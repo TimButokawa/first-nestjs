728dc1cef11831acbb60c0f8ad8b68ec4c206e69
Tim Butorac <timbutorac@gmail.com>
2024-05-27 09:16:07 -0700

    explore controller routes
    controller level route
    get level route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 
 // App Controller
-@Controller()
+// Defining a path here will apply it to all routes in the controller
+// For example, the path here is '/app' so all routes in this controller will be prefixed with '/app'
+// e.g. '/app/hello-world'
+@Controller('/app')
 export class AppController {
-  // Get methods
-  @Get()
+  // /app/hello-world
+  @Get('/hello-world')
   getRootRoute() {
     return 'Hello World';
+  }
+  // /app/hello
+  @Get('/hello')
+  getHelloRoute() {
+    return 'Hello';
   }
 }
